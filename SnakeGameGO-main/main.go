f4e0b4964b63fb0880ac4b3801eaa7e5e404ab59
Bastián Contreras Wasilkowski <91445987+Wasil-Was@users.noreply.github.com>
2022-12-15 16:16:09 -0300

    Update main.go

--- SnakeGameGO-main/main.go
+++ SnakeGameGO-main/main.go
@@ -4,17 +4,29 @@ package main
 // REGLAS BÁSICAS:
 Grilla de tamaño N×M.
 Pueden haber 'p' serpientes.
-Las serpientes no pueden transitar por una celda ocupada por otra serpiente.
-Cada serpiente se desplaza de forma autónoma intentando llegar primero que las demás al alimento.
 Cada vez que una serpiente alcance el alimento, ésta crecerá y, a la vez, deberá surgir una nueva porción de comida en otra celda (definida aleatoriamente).
 Cada 'x' segundos cada serpiente se mueve una celda.
+
+Cada serpiente se desplaza de forma autónoma intentando llegar primero que las demás al alimento.
+Las serpientes no pueden transitar por una celda ocupada por otra serpiente.
 El juego debe continuar hasta que las serpientes no puedan seguir moviéndose.
+
 // REGLAS CON CONCURRENCIA:
 Las serpientes deben ser representadas por un Thread (liviano o pesado) independiente (con exclusion mutua).
 Si una serpiente no puede moverse entra a deadlock permanente (o momentaneo).
 El programa debe ser tan asíncrono y libre de barreras de sincronización como sea posible.
+
 // GRILLA:
 Defina un mecanismo que permita observar los diferentes estados de las serpientes en la grilla.
+
+// OPCIONAL:
+Hacer que la matriz se refresque bien (Retorno de Carro)
+
+Movimiento serpientes:
+	0 = arriba
+	1 = abajo
+	2 = izquierda
+	3 = derecha
 */
 
 import (
@@ -25,15 +37,17 @@ import (
 	"time"
 )
 
-// MAIN------------------------------------------------------------------------------------------------------------------------------------------------
-var ancho, largo int //Tamaño de la grilla.
-var Dir byte         //Direccion de la serpiente.
-var oldDir byte      //Direccion de la serpiente.
-var posComida Coords //Coordenadas de comida
-var list []Snake     // Lista serpientes
-var wg sync.WaitGroup
+// DEFINICIONES ------------------------------------------------------------------------------------------------------------------------------------------------
+var ancho, largo int // Tamaño de la grilla.
+var vel int          // Tamaño de la grilla.
+var cantSerp int     // Tamaño de la grilla.
+var Dir byte         // Direccion de la serpiente.
+var oldDir byte      // Direccion de la serpiente.
+var posComida Coords // Coordenadas de comida.
+var list []Snake     // Lista serpientes.
+var w sync.WaitGroup // Esperar goroutines.
 
-type MyGrilla [][]string //Grilla
+type MyGrilla [][]string // Grilla
 
 func (grilla MyGrilla) String() string {
 	out := "█"
@@ -77,10 +91,7 @@ func (s *Snake) quitarCola() Coords {
 	return temp
 }
 
-//RETORNO DE CARRO GOLANG (hacer que la matriz se refresque bonito)
-
-// FUNCIONES------------------------------------------------------------------------------------------------------------------------------------------------
-
+// FUNCIONES ------------------------------------------------------------------------------------------------------------------------------------------------
 func colocarComida(grilla MyGrilla) {
 	seed := rand.NewSource(time.Now().UnixNano())
 	random := rand.New(seed)
@@ -93,7 +104,6 @@ func colocarComida(grilla MyGrilla) {
 	posComida.Y = y
 }
 
-// 0 arriba;		1 abajo;	2 izquierda;	3 derecha
 func verificar() {
 	n, o := Dir, oldDir
 	if (n == 0 && o == 1) || (n == 1 && o == 0) || (n == 3 && o == 2) || (n == 2 && o == 3) {
@@ -118,7 +128,6 @@ func celdaSig(coord Coords, dir byte, grilla MyGrilla) (Coords, bool) {
 		tempCoord.Y += 1
 		break
 	}
-
 	// Golpear la pared
 	if tempCoord.X >= ancho || tempCoord.Y >= largo || tempCoord.X < 0 || tempCoord.Y < 0 {
 		return tempCoord, false
@@ -133,14 +142,11 @@ func celdaSig(coord Coords, dir byte, grilla MyGrilla) (Coords, bool) {
 func actualizarGrilla(grilla MyGrilla, snake *Snake) {
 	last := int(len(snake.Cola) - 1)
 	newCell, cont := celdaSig(snake.Cola[last], Dir, grilla)
-
 	if !cont {
 		snake.lost = true
 		return
 	}
-
 	snake.agregarCola(newCell)
-
 	last = len(snake.Cola) - 1
 	grilla[snake.Cola[last].X][snake.Cola[last].Y] = "□"
 
@@ -153,103 +159,99 @@ func actualizarGrilla(grilla MyGrilla, snake *Snake) {
 	oldDir = Dir
 }
 
-// MAIN------------------------------------------------------------------------------------------------------------------------------------------------
+// MAIN ------------------------------------------------------------------------------------------------------------------------------------------------
 func main() {
-	//Argumentos
-	ancho1 := flag.Int("ancho", 0, "El ancho de la grilla.")
-	largo1 := flag.Int("largo", 18, "El largo de la grilla.")
-	vel1 := flag.Int("velocidad", 20, "La velocidad de la grilla.")
+	// Argumentos
+	ancho1 := flag.Int("ancho", 10, "El ancho de la grilla.")
+	largo1 := flag.Int("largo", 25, "El largo de la grilla.")
+	vel1 := flag.Int("velocidad", 100, "La velocidad de la grilla.")
+	cantSerp1 := flag.Int("serpientes", 1, "Cantidad de Serpientes.")
 	flag.Parse()
-	fmt.Println("Ancho = ", *ancho1)
-	fmt.Println("Largo = ", *largo1)
-	fmt.Println("Velocidad = ", *vel1)
-	cant_snake := 3
-	wg.Add(3)
-	//Dimensiones de la grilla
-	ancho, largo = 10, 25
 
-	//Grilla Vacia
+	// Dimensiones de la grilla
+	ancho, largo = *ancho1, *largo1
+	// Velocidad
+	vel = *vel1
+	// Cantidad de Serpientes
+	cantSerp = *cantSerp1
+
+	// Grilla Vacia
 	var grilla MyGrilla
 	grilla = make([][]string, ancho)
 	for i := 0; i < int(ancho); i++ {
 		grilla[i] = make([]string, largo)
 	}
 
-	//Inicializar Grilla
-	for i := 0; i < int(ancho); i++ {
-		for j := 0; j < int(largo); j++ {
+	// Inicializar Grilla
+	for i := 0; i < ancho; i++ {
+		for j := 0; j < largo; j++ {
 			grilla[i][j] = string(" ")
 		}
 	}
-	for i := 0; i <= int(cant_snake); i++ {
-		// LA POSICIÓN DE LA SERPIENTE NO DEBE CREAR UNA NUEVA GRILLA
-		var s Snake
 
+	// Inicializar Serpientes
+	for i := 0; i < cantSerp; i++ {
+		var s Snake
 		s.lost = false
 		time.Sleep(time.Millisecond * time.Duration(100))
 		rand.Seed(time.Now().UnixNano())
 		serpX, serpY := rand.Intn(int(ancho-1)), rand.Intn(int(largo-1))
 		s.agregarCola(Coords{serpX, serpY + 1})
 		grilla[serpX][serpY+1] = "□"
-		// SE AGREGA LA SERPIENTE A LA VARIABLE LIST
 		list = append(list, s)
 	}
-	//Inicializar serpientes.
-	for i := 0; i < int(cant_snake); i++ {
-		ch1 := make(chan byte)
 
-		go func(ch1 chan byte) {
+	// Colocar comida en la grilla
+	colocarComida(grilla)
+	fmt.Println(grilla)
+
+	// Goroutines serpientes
+	ch := make(chan byte)
+	for i := 0; i < cantSerp; i++ {
+		w.Add(1)
+		go func(i int, co chan byte) {
 			//Movimiento serpiente
 			for {
-
-				last := int(len((list[i]).Cola) - 1)
+				last := int(len(list[i].Cola) - 1)
 				time.Sleep(time.Millisecond * time.Duration(100))
 				if posComida.X > list[i].Cola[last].X {
 					time.Sleep(time.Millisecond * time.Duration(100))
 					if Dir == 0 && list[i].Cola[last].Y-1 > 0 && (grilla[list[i].Cola[last].X][list[i].Cola[last].Y-1] == " " || grilla[list[i].Cola[last].X][list[i].Cola[last].Y-1] == "◈") {
 						oldDir = 0
-						ch1 <- 2
+						co <- 2
 					} else if Dir == 0 && list[i].Cola[last].Y-1 == 0 && (grilla[list[i].Cola[last].X][list[i].Cola[last].Y+1] == " " || grilla[list[i].Cola[last].X][list[i].Cola[last].Y+1] == "◈") {
 						oldDir = 0
-						ch1 <- 3
+						co <- 3
 					} else if Dir == 0 && (grilla[list[i].Cola[last].X+1][list[i].Cola[last].Y] == " " || grilla[list[i].Cola[last].X+1][list[i].Cola[last].Y] == "◈") {
 						oldDir = 0
-						ch1 <- 0
+						co <- 0
 					} else {
-						ch1 <- 1
+						co <- 1
 					}
 				} else if posComida.X < list[i].Cola[last].X {
 					time.Sleep(time.Millisecond * time.Duration(100))
-					ch1 <- 0
+					co <- 0
 				} else if posComida.X == list[i].Cola[last].X {
 					time.Sleep(time.Millisecond * time.Duration(100))
 					if posComida.Y > list[i].Cola[last].Y {
-						//Estos aun estan con pruebas
-						//Condiciones para "evitar" Deadlock
-						//if Dir == 2 && snakes[i].Cola[last].X-1 > 0 && grilla[snakes[i].Cola[last].X-1][snakes[i].Cola[last].Y] == "" {
 						if Dir == 2 && list[i].Cola[last].X-1 > 0 && (grilla[list[i].Cola[last].X-1][list[i].Cola[last].Y] == " " || grilla[list[i].Cola[last].X-1][list[i].Cola[last].Y] == "◈") {
-							fmt.Println("<- to up")
 							oldDir = 2
-							ch1 <- 0
+							co <- 0
 						} else if Dir == 2 && list[i].Cola[last].X-1 == 0 && (grilla[list[i].Cola[last].X+1][list[i].Cola[last].Y] == " " || grilla[list[i].Cola[last].X+1][list[i].Cola[last].Y] == "◈") {
-							fmt.Println("<- to down")
 							oldDir = 2
-							ch1 <- 1
+							co <- 1
 						} else {
-							ch1 <- 3
+							co <- 3
 						}
-
 					} else if posComida.Y < list[i].Cola[last].Y {
 						if Dir == 3 && list[i].Cola[last].X-1 > 0 && (grilla[list[i].Cola[last].X-1][list[i].Cola[last].Y] == " " || grilla[list[i].Cola[last].X-1][list[i].Cola[last].Y] == "◈") {
-							fmt.Println("-> to up")
 							oldDir = 3
-							ch1 <- 0
+							co <- 0
 						} else if Dir == 3 && list[i].Cola[last].X-1 == 0 && (grilla[list[i].Cola[last].X+1][list[i].Cola[last].Y] == " " || grilla[list[i].Cola[last].X+1][list[i].Cola[last].Y] == "◈") {
-							fmt.Println("-> to down")
 							oldDir = 3
-							ch1 <- 1
+							co <- 1
 						} else {
-							ch1 <- 2
+							co <- 2
 						}
 					}
 				}
@@ -259,29 +261,34 @@ func main() {
 					}
 				}
 			}
-		}(ch1)
-
-		//Colocar comida en la grilla
-		colocarComida(grilla)
-		fmt.Println(grilla)
-
-		//Ejecución del juego
-		for {
-			//Direccion serpiente
-			select {
-			case stdin, _ := <-ch1:
-				Dir = stdin
-				verificar()
+		}(i, ch)
+	}
+	// Ejecucion del Juego
+	for {
+		// Direccion serpiente
+		select {
+		case stdin, _ := <-ch:
+			Dir = stdin
+			verificar()
+			for i := 0; i < len(list); i++ {
 				actualizarGrilla(grilla, &list[i])
 				time.Sleep(time.Millisecond * time.Duration(100))
-
-			}
-			time.Sleep(time.Millisecond * time.Duration(200))
-			fmt.Println(grilla) //Imprimir pasos de la grilla
-			if list[i].lost == true {
-				fmt.Println("Todas las serpientes han muerto.")
-				break
 			}
 		}
+		// Imprimir pasos de la grilla
+		time.Sleep(time.Millisecond * time.Duration(200))
+		fmt.Println(grilla)
+		// Verificar fin del juego
+		contar_Muertas := 0
+		for i := 0; i < len(list); i++ {
+			if list[i].lost == true {
+				contar_Muertas++
+			}
+		}
+		if contar_Muertas >= cantSerp {
+			fmt.Println("Todas las serpientes han muerto.")
+			break
+		}
 	}
+	close(ch)
 }
